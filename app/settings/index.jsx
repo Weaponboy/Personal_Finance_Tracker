0fcd6fbf1417420492b7fccd02c40c80bfd73a33
Weaponboy <joshandy06@gmail.com>
2025-09-24 08:41:11 +0200

    Added logout button

--- app/settings/index.jsx
+++ app/settings/index.jsx
@@ -1,8 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { useRouter } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { logout, useAuth } from '../../lib/AuthContext';
 
 
 const Settings = () => {
-    return <View style={StyleSheet.container}></View>
+
+    const router = useRouter();
+    const { user, loading } = useAuth();
+
+    const handleLogout = async () => {
+        console.log('Attempting logout');
+        await logout();
+        // Redirect to login manually if needed (optional, as useEffect in Login will handle it)
+        if (!loading && !user) {
+            console.log('Redirecting to login after logout');
+            router.replace('/login');
+        }
+    };
+
+    if (loading) {
+        return <View style={styles.container}><Text>Loading...</Text></View>;
+    }
+
+    return <View style={styles.container}>
+        <TouchableOpacity style={styles.logout} onPress={handleLogout}>
+            <Text style={styles.logoutText}>Logout</Text>
+        </TouchableOpacity>
+
+    </View>
 }
 
 const styles = StyleSheet.create({
@@ -13,7 +38,17 @@ const styles = StyleSheet.create({
         paddingBottom: 90,
         backgroundColor: '#fff'
     },
-
+    logout: {
+        position: 'absolute',
+        top: 15,
+        right: 15,
+        padding: 10,
+        backgroundColor: "#000000",
+        borderRadius: 12
+    },
+    logoutText: {
+        color: '#fff'
+    }
 })
 
 export default Settings;
